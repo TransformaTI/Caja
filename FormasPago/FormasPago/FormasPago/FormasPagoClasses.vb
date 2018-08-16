01d83c6801d53212ede7fc32e0b04c1930790920
Iván Trujillo <itrujillo@softgrade.mx>
2018-08-16 09:38:56 -0500

    Ajustes para tener dos formularios de tipo de cobro para eficiencias negativas

---
+++ FormasPago/FormasPago/FormasPago/FormasPagoClasses.vb
@@ -0,0 +1,34 @@
+﻿Public Class DebitoAnticipo
+    Public _anio As String
+    Public _folio As String
+    Public _montodebitado As Decimal
+
+    Property anio() As String
+        Get
+            Return _anio
+        End Get
+        Set(ByVal Value As String)
+            _anio = Value
+        End Set
+    End Property
+
+    Property folio() As String
+        Get
+            Return _folio
+        End Get
+        Set(ByVal Value As String)
+            _folio = Value
+        End Set
+    End Property
+
+    Property montodebitado() As Decimal
+        Get
+            Return _montodebitado
+        End Get
+        Set(ByVal Value As Decimal)
+            _montodebitado = Value
+        End Set
+    End Property
+
+
+End Class
--- FormasPago/FormasPago/FormasPago/frmSelTipoCobro.vb
+++ FormasPago/FormasPago/FormasPago/frmSelTipoCobro.vb
@@ -2950,39 +2950,6 @@ Public Class frmSelTipoCobro
     End Sub
 End Class
 
-Public Class DebitoAnticipo
-    Public _anio As String
-    Public _folio As String
-    Public _montodebitado As Decimal
 
-    Property anio() As String
-        Get
-            Return _anio
-        End Get
-        Set(ByVal Value As String)
-            _anio = Value
-        End Set
-    End Property
-
-    Property folio() As String
-        Get
-            Return _folio
-        End Get
-        Set(ByVal Value As String)
-            _folio = Value
-        End Set
-    End Property
-
-    Property montodebitado() As Decimal
-        Get
-            Return _montodebitado
-        End Get
-        Set(ByVal Value As Decimal)
-            _montodebitado = Value
-        End Set
-    End Property
-
-
-End Class
 
 
